beb6f362afb07863855223883afbe0540641c0b1
Mason Bulling <42184555+mbulling@users.noreply.github.com>
2022-11-26 21:33:14 -0500

    Remove build files from master - Mason Bulling (mcb343)

--- _build/default/src/destringify.mli
+++
@@ -1,9 +0,0 @@
-open Dbtype
-
-exception InvalidDB
-(**An exception of type [InvalidDB] is raised when an invalid .json file is
-   passed in to the [LOAD] command*)
-
-val read_file : string -> db
-(**[read_file file] is the database of type [db] represented in the json file
-   [file]. Raises [InvalidDB] if the file cannot be parsed as a database*)
