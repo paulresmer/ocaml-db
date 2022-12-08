590c754133f9dab28d41eb439af5f6181a24b5a6
andrew <asc269@cornell.edu>
2022-12-07 23:35:42 -0500

    Remove _build files

--- _build/default/src/cloud.mli
+++
@@ -1,9 +0,0 @@
-open Dbtype
-
-val save_to_cloud : db -> unit
-(**[save_to_cloud db] stringifies the database [db] and saves it to a remote
-   URL, using the JSONBINS API*)
-
-val download : string -> db
-(**[download id] looks for a database with id [id]. If it exists, it downloads
-   the JSON and parses it to a db.*)
